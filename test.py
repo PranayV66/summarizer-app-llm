48f2e029b8fba492e920b22135b1afabe9471475
root <root@ip-172-31-88-91.ec2.internal>
2025-01-07 21:32:31 +0000

    Updating test script to work with the new port

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 from requests import post 
 from json import loads
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+llm_port = os.environ.get('LLM_CONTAINER_PORT')
+url = "http://localhost:" + llm_port +"/api/generate"
 response_text = ""
-for chunk in post(url="http://localhost:3000/api/generate", json={"model": "llama3.2", "prompt": "What can you do?"}).text.splitlines():
-    # print (type(loads(chunk)["response"]))
+for chunk in post(url, json={"model": "llama3.2", "prompt": "What can you do?"}).text.splitlines():
     response_text += loads(chunk)["response"]
 print(response_text)
