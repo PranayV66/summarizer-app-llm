7289aabab06e21dc353488c6c811ba6fbc23b0e8
root <root@ip-172-31-88-91.ec2.internal>
2025-01-29 21:00:54 +0000

    Changed model to mistral

--- start.sh
+++ start.sh
@@ -5,11 +5,11 @@ nohup ollama serve &
 
 sleep 10
 
-echo "Pulling model deepseek-r1:1.5b..."
-ollama pull deepseek-r1:1.5b
+echo "Pulling model mistral..."
+ollama pull mistral
 
 echo "Stopping background Ollama server..."
 pkill ollama
 
-echo "Starting Ollama server with deepseek-r1:1.5b..."
+echo "Starting Ollama server with mistral..."
 exec ollama serve
